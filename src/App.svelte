211485b90e94cfc012c02336f100544bff5742e4
Juri Wulff <juriwulffonline@gmail.com>
2021-03-07 16:01:34 +0100

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -7,23 +7,20 @@
 
     let user;
 
-    const unsubscribe = authState(auth).subscribe(u => user = u);
+    const unsubscribe = authState(auth).subscribe((u) => (user = u));
 
     function login() {
         auth.signInWithPopup(googleProvider);
     }
 </script>
 
-
 <section>
-{#if user}
-    <Profile {...user} />
-    <button on:click={ () => auth.signOut() }>Logout</button>
-    <hr>
-    <Todos uid={user.uid} />
-{:else}
-	<button on:click={login}>
-		Signin with Google
-	</button>
-{/if}
+    {#if user}
+        <Profile {...user} />
+        <button on:click={() => auth.signOut()}>Logout</button>
+        <hr />
+        <Todos uid={user.uid} />
+    {:else}
+        <button on:click={login}> Signin with Google </button>
+    {/if}
 </section>
